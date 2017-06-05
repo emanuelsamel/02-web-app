d600e7f115de9be5858195fa7c19b1281312a0da
emanuelsamel <mannys94@hotmail.com>
2017-06-05 19:19:21 -0400

    final commit

--- crm.rb
+++ crm.rb
@@ -3,7 +3,7 @@ require_relative 'contact'
 require 'sinatra'
 require 'sinatra/reloader'
 get "/"do
-  erb :home
+erb :home
 end
 get "/contacts" do
   @contacts = Contact.all
@@ -23,21 +23,57 @@ get "/contacts/:id" do
   @contact = Contact.find_by(id: params[:id])
   if
     @contact
-  erb :show_contact
+    erb :show_contact
   else
     raise Sinatra::NotFound
   end
 end
 
-post "/create" do
+post "/contacts" do
   Contact.create(
-      first_name: params[:first_name],
-      last_name:  params[:last_name],
-      email:      params[:email],
-      note:       params[:note]
-    )
-    redirect "/contacts"
+  first_name: params[:first_name],
+  last_name:  params[:last_name],
+  email:      params[:email],
+  note:       params[:note]
+  )
+  redirect "/contacts"
 end
+
+get '/contacts/:id/edit' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find_by(id: params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+delete '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.delete
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 after do
   ActiveRecord::Base.connection.close
 end
